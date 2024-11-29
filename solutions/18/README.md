d55a0c8e43a437a827473f8568730c1bb251beb4
Rob Habraken <roberthabraken@hotmail.com>
2024-11-29 17:10:35 +0100

    Add puzzle solution scaffolding for all days upfront

---
+++ solutions/18/README.md
@@ -0,0 +1,4 @@
+*This is a copy of the original puzzle description. If you want to learn more about my approach to solving these puzzles see [my solutions](solutions.md).*
+
+---
+# Day 18: 
---
+++ solutions/20/part-1/Program.cs
@@ -0,0 +1,9 @@
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\20\\input.example");
+
+var answer = 0;
+foreach (var line in lines)
+{
+
+}
+
+Console.WriteLine(answer);
