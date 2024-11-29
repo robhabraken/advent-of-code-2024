3e037a6199a0ca955b60230b17f8fa4a1ca1bd9b
Rob Habraken <roberthabraken@hotmail.com>
2024-11-29 16:27:48 +0100

    Setup repo

---
+++ solutions/01/part-1/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\01\\input.example");
+
+var answer = 0;
+foreach (var line in lines)
+{
+
+}
+
+Console.WriteLine(answer);
