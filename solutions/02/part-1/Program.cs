3d62987bec71e0055c3793be6413c9dde14d6ef2
Rob Habraken <roberthabraken@hotmail.com>
2024-12-02 12:40:01 +0100

    Add solutions to day 2

--- solutions/02/part-1/Program.cs
+++ solutions/02/part-1/Program.cs
@@ -1,9 +1,35 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\02\\input.example");
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\02\\input.txt");
 
 var answer = 0;
 foreach (var line in lines)
 {
-
+    var report = line.Split(' ').Select(int.Parse).ToArray();
+    if (isSafe(report))
+        answer++;
 }
 
 Console.WriteLine(answer);
+
+static bool isSafe(int[] report)
+{
+    var currentLevel = report[0];
+    var direction = 0;
+
+    for (int i = 1; i < report.Length; i++)
+    {
+        var delta = report[i] - currentLevel;
+
+        if (delta == 0 || delta > 3 || delta < -3)
+            return false;
+
+        if (direction == 0)
+            direction = delta;
+        else
+            if ((direction > 0 && delta < 0) || (direction < 0 && delta > 0))
+                return false;
+
+        currentLevel = report[i];
+    }
+
+    return true;
+}
--- solutions/02/part-2/Program.cs
+++ solutions/02/part-2/Program.cs
@@ -1,9 +1,52 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\02\\input.example");
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\02\\input.txt");
 
 var answer = 0;
 foreach (var line in lines)
 {
-
+    var report = line.Split(' ').Select(int.Parse).ToArray();
+    if (isSafe(report, true))
+        answer++;
 }
 
 Console.WriteLine(answer);
+
+static bool isSafe(int[] report, bool problemDampener)
+{
+    var ups = 0;
+    var downs = 0;
+    var zeros = 0;
+    var nboWrongSteps = 0;
+
+    for (var i = 1; i < report.Length; i++)
+    {
+        if (report[i] > report[i - 1])
+            ups++;
+        else if (report[i] < report[i - 1])
+            downs++;
+        else
+            zeros++;
+
+        var diff = Math.Abs(report[i] - report[i - 1]);
+        if (diff == 0 || diff > 3)
+            nboWrongSteps++;
+    }
+
+    if ((ups == 0 || downs == 0) && zeros == 0 && nboWrongSteps == 0)
+        return true;
+
+    if (problemDampener)
+    {
+        for (var indexToRemove = 0; indexToRemove < report.Length; indexToRemove++)
+        { 
+            var newReport = new List<int>();
+            for (var i = 0; i < report.Length; i++)
+                if (i != indexToRemove)
+                    newReport.Add(report[i]);
+
+            if (isSafe([.. newReport], false))
+                return true;
+        }
+    }
+    
+    return false;
+}
