5791e2541313d68c852eea3b2665b110eaf4d65e
Rob Habraken <roberthabraken@hotmail.com>
2024-12-03 10:19:37 +0100

    Add solution to day 3 part 1

--- solutions/03/part-1/Program.cs
+++ solutions/03/part-1/Program.cs
@@ -1,9 +1,15 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\03\\input.example");
+using System.Text.RegularExpressions;
+
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\03\\input.txt");
+
+var regex = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
 
 var answer = 0;
 foreach (var line in lines)
-{
-
-}
+    foreach (var match in regex.Matches(line))
+    {
+        var numbers = $"{match}"[4..^1].Split(',').Select(int.Parse).ToArray();
+        answer += numbers[0] * numbers[1];
+    }
 
 Console.WriteLine(answer);
