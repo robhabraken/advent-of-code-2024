5b75612a6c665e30f5a52f23d73a720ecef30da7
Rob Habraken <roberthabraken@hotmail.com>
2024-12-03 11:35:29 +0100

    Clean up regex for day 3

--- solutions/03/part-1/Program.cs
+++ solutions/03/part-1/Program.cs
@@ -2,7 +2,7 @@ using System.Text.RegularExpressions;
 
 string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\03\\input.txt");
 
-var regex = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
+var regex = new Regex(@"mul\(\d{1,3},\d{1,3}\)");
 
 var answer = 0;
 foreach (var line in lines)
