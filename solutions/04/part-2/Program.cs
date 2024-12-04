ac52fa670aabd102284636817d4d1164cf14a8c4
Rob Habraken <roberthabraken@hotmail.com>
2024-12-04 12:56:26 +0100

    Cleaned up day 4 part 2

--- solutions/04/part-2/Program.cs
+++ solutions/04/part-2/Program.cs
@@ -1,18 +1,15 @@
 string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\04\\input.txt");
 
 var answer = 0;
-for (var line = 1; line < lines.Length - 1; line++)
-    for (var character = 1; character < lines[line].Length - 1; character++)
-        if (lines[line][character].Equals('A'))
-            Search(line, character);
+for (var y = 1; y < lines.Length - 1; y++)
+    for (var x = 1; x < lines[y].Length - 1; x++)
+        if (lines[y][x].Equals('A'))
+        {
+            var d1 = $"{lines[y - 1][x - 1]}A{lines[y + 1][x + 1]}";
+            var d2 = $"{lines[y + 1][x - 1]}A{lines[y - 1][x + 1]}";
+
+            if ((d1.Equals("SAM") || d1.Equals("MAS")) && (d2.Equals("SAM") || d2.Equals("MAS")))
+                answer++;
+        }
 
 Console.WriteLine(answer);
-
-void Search(int y, int x)
-{
-    var d1 = $"{lines[y - 1][x - 1]}A{lines[y + 1][x + 1]}";
-    var d2 = $"{lines[y + 1][x - 1]}A{lines[y - 1][x + 1]}";
-
-    if ((d1.Equals("SAM") || d1.Equals("MAS")) && (d2.Equals("SAM") || d2.Equals("MAS")))
-        answer++;
-}
