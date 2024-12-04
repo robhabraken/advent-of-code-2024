026f0998609d994d2bcb4f1dc66fd057bd2d87d3
Rob Habraken <roberthabraken@hotmail.com>
2024-12-04 09:47:14 +0100

    Add solution to day 4 part 2

--- solutions/04/part-2/Program.cs
+++ solutions/04/part-2/Program.cs
@@ -1,9 +1,18 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\04\\input.example");
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\04\\input.txt");
 
 var answer = 0;
-foreach (var line in lines)
-{
-
-}
+for (var line = 1; line < lines.Length - 1; line++)
+    for (var character = 1; character < lines[line].Length - 1; character++)
+        if (lines[line][character].Equals('A'))
+            Search(line, character);
 
 Console.WriteLine(answer);
+
+void Search(int y, int x)
+{
+    var d1 = $"{lines[y - 1][x - 1]}A{lines[y + 1][x + 1]}";
+    var d2 = $"{lines[y + 1][x - 1]}A{lines[y - 1][x + 1]}";
+
+    if ((d1.Equals("SAM") || d1.Equals("MAS")) && (d2.Equals("SAM") || d2.Equals("MAS")))
+        answer++;
+}
