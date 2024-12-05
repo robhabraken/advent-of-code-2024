5835eb7baf286483f6aae022d94435ca55fc4cd6
Rob Habraken <roberthabraken@hotmail.com>
2024-12-05 11:33:04 +0100

    Optimize day 5 part 2

--- solutions/05/part-2/Program.cs
+++ solutions/05/part-2/Program.cs
@@ -1,37 +1,36 @@
 string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\05\\input.txt");
 
-var rules = new List<int[]>();
+var r = new Dictionary<int, List<int>>();
 
 var answer = 0;
 foreach (var line in lines)
     if (line.Contains('|'))
-        rules.Add(line.Split('|').Select(int.Parse).ToArray());
+        AddRule(line.Split('|').Select(int.Parse).ToArray());
     else if (!string.IsNullOrEmpty(line))
-        CheckUpdate(line.Split(',').Select(int.Parse).ToList());
+        CheckUpdate(line);
 
 Console.WriteLine(answer);
 
-void CheckUpdate(List<int> numbers)
+void AddRule(int[] numbers)
 {
-    foreach (var rule in rules)
-        if (numbers.Contains(rule[0]) && numbers.Contains(rule[1]))
-            if (numbers.IndexOf(rule[0]) > numbers.IndexOf(rule[1]))
-                CorrectUpdate(numbers);  
+    if (!r.ContainsKey(numbers[0]))
+        r.Add(numbers[0], []);
+
+    r[numbers[0]].Add(numbers[1]);
 }
 
-void CorrectUpdate(List<int> numbers)
+void CheckUpdate(string numbers)
 {
-    numbers.Sort(CompareNumbers);
-    answer += numbers[numbers.Count / 2];
+    var list = numbers.Split(',').Select(int.Parse).ToList();
+    list.Sort(CompareNumbers);
+    if (!numbers.Equals(string.Join(",", list)))
+        answer += list[list.Count / 2];
 }
 
 int CompareNumbers(int a, int b)
 {
-    foreach (var rule in rules)
-        if (rule[0] == a && rule[1] == b)
-            return -1;
-        else if (rule[1] == a && rule[0] == b)
-            return 1;
+    if (r.TryGetValue(a, out List<int>? value) && value.Contains(b))
+        return -1;
 
-    return 0;
+    return 1;
 }
