54adc8a743e1d1ebe47d8eaba2dc28b153a2ba86
Rob Habraken <roberthabraken@hotmail.com>
2024-12-06 10:13:31 +0100

    Add solution to day 6 part 2

--- solutions/06/part-2/Program.cs
+++ solutions/06/part-2/Program.cs
@@ -1,9 +1,51 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\06\\input.example");
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\06\\input.txt");
+
+(int, int) location = (0, 0);
+var referenceMap = new bool[lines.Length, lines[0].Length];
+var deltaMap = new int[4, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
 var answer = 0;
-foreach (var line in lines)
-{
+for (int y = 0; y < lines.Length; y++)
+    if (lines[y].Contains('^'))
+        location = (y, lines[y].IndexOf('^'));
 
-}
+Walk(ref referenceMap, (location.Item1, location.Item2), -1, -1);
+
+var testMap = new bool[lines.Length, lines[0].Length];
+for (int y = 0; y < lines.Length; y++)
+    for (int x = 0; x < lines[0].Length; x++)
+        if (referenceMap[y, x])
+            if (Walk(ref testMap, (location.Item1, location.Item2), y, x))
+                answer++;
 
 Console.WriteLine(answer);
+
+bool Walk(ref bool[,] map, (int, int) location, int obstacleY, int obstacleX)
+{
+    var visitedDirections = new bool[lines.Length, lines[0].Length, 4];
+    var direction = 0;
+
+    while (true)
+    {
+        map[location.Item1, location.Item2] = true;
+
+        visitedDirections[location.Item1, location.Item2, direction % 4] = true;
+
+        var dY = location.Item1 + deltaMap[direction % 4, 0];
+        var dX = location.Item2 + deltaMap[direction % 4, 1];
+
+        if (dY < 0 || dY >= lines.Length || dX < 0 || dX >= lines[0].Length)
+            return false; // walking off of the grid
+
+        if (visitedDirections[dY, dX, direction % 4])
+            return true; // loop detected
+
+        if (lines[dY][dX].Equals('#') || (dY == obstacleY && dX == obstacleX))
+            direction++;
+        else
+        {
+            location.Item1 = dY;
+            location.Item2 = dX;
+        }
+    }
+}
