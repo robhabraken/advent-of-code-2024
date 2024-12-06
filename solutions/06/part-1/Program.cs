1fe988bb75febef37325d2082a684f55eb158633
Rob Habraken <roberthabraken@hotmail.com>
2024-12-06 08:58:21 +0100

    Add solution to day 6 part 1

--- solutions/06/part-1/Program.cs
+++ solutions/06/part-1/Program.cs
@@ -1,9 +1,37 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\06\\input.example");
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\06\\input.txt");
+
+var map = new bool[lines.Length, lines[0].Length];
+(int, int) location = (0, 0);
+var direction = 0;
+var deltaMap = new int[4, 2] {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
 var answer = 0;
-foreach (var line in lines)
-{
+for (int y = 0; y < lines.Length; y++)
+    if (lines[y].Contains('^'))
+        location = (y, lines[y].IndexOf('^'));
 
+while (true)
+{
+    map[location.Item1, location.Item2] = true;
+
+    var dY = location.Item1 + deltaMap[direction % 4, 0];
+    var dX = location.Item2 + deltaMap[direction % 4, 1];
+
+    if (dY < 0 || dY >= lines.Length || dX < 0 || dX >= lines[0].Length)
+        break;
+
+    if (lines[dY][dX].Equals('#'))
+        direction++;
+    else
+    {
+        location.Item1 = dY;
+        location.Item2 = dX;
+    }
 }
+
+for (int y = 0; y < lines.Length; y++)
+    for (int x = 0; x < lines[0].Length; x++)
+        if (map[y, x])
+            answer++;
 
 Console.WriteLine(answer);
