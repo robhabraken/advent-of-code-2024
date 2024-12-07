294cbd8d002a0da3c4ec99f865eb99df32b65133
Rob Habraken <roberthabraken@hotmail.com>
2024-12-07 10:55:19 +0100

    Apply new part 2 solution to part 1 for better performance

--- solutions/07/part-1/Program.cs
+++ solutions/07/part-1/Program.cs
@@ -1,37 +1,36 @@
 string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\07\\input.txt");
 
-var combinations = new List<List<string>>();
-for (var operatorCount = 0; operatorCount < 12; operatorCount++)
-{
-    combinations.Add([]);
-    for (var i = 0; i < (int)Math.Pow(2, operatorCount) && operatorCount > 0; i++)
-        combinations[operatorCount].Add(Convert.ToString(i, 2).PadLeft(operatorCount, '0').Replace('0', '*').Replace('1', '+'));
-}
+var ops = new char[] { '+', '*' };
 
 long answer = 0;
 foreach (var line in lines)
 {
     var equation = line.Split(':');
-
     var testValue = long.Parse(equation[0]);
     var numbers = equation[1].Trim().Split(' ').Select(long.Parse).ToArray();
 
-    foreach (var operators in combinations[numbers.Length - 1])
-    {
-        long result = numbers[0];
-        for (var i = 0; i < operators.Length; i++)
-        {
-            if (operators[i].Equals('+'))
-                result += numbers[i + 1];
-            else
-                result *= numbers[i + 1];
-        }
-        if (result == testValue)
-        {
-            answer += result;
-            break;
-        }
-    }
+    var possiblyTrue = false;
+    Evaluate(testValue, numbers[0], numbers, 1, ref possiblyTrue);
+
+    if (possiblyTrue)
+        answer += testValue;
 }
 
 Console.WriteLine(answer);
+
+void Evaluate(long testValue, long current, long[] numbers, int index, ref bool possiblyTrue)
+{
+    if (!possiblyTrue)
+        foreach (var op in ops)
+        {
+            var result = current + numbers[index];
+            if (op.Equals('*'))
+                result = current * numbers[index];
+
+            if (index == numbers.Length - 1 && result == testValue)
+                possiblyTrue = true;
+
+            if (index < numbers.Length - 1 && result <= testValue)
+                Evaluate(testValue, result, numbers, index + 1, ref possiblyTrue);
+        }
+}
