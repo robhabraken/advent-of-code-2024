e823fae055e09fb5bf1df1d4119f6917458ae522
Rob Habraken <roberthabraken@hotmail.com>
2024-12-08 16:13:42 +0100

    Optimize day 8 by moving everything in a single pass loop

--- solutions/08/part-2/Program.cs
+++ solutions/08/part-2/Program.cs
@@ -1,6 +1,7 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\08\\input.txt");
+var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\08\\input.txt");
 
 var antennas = new Dictionary<char, List<Tuple<int, int>>>();
+var antinodes = new HashSet<Tuple<int, int>>();
 
 for (var y = 0; y < lines.Length; y++)
     for (var x = 0; x < lines[0].Length; x++)
@@ -8,19 +9,17 @@ for (var y = 0; y < lines.Length; y++)
             if (!antennas.TryGetValue(lines[y][x], out List<Tuple<int, int>>? value))
                 antennas.Add(lines[y][x], [new Tuple<int, int>(y, x)]);
             else
-                value.Add(new Tuple<int, int>(y, x));
-
-var antinodes = new HashSet<Tuple<int, int>>();
-
-foreach (var frequency in antennas.Keys)
-    for (var i = 0; i < antennas[frequency].Count; i++)
-        for (var j = i + 1; j < antennas[frequency].Count; j++)
             {
-                var dY = antennas[frequency][i].Item1 - antennas[frequency][j].Item1;
-                var dX = antennas[frequency][i].Item2 - antennas[frequency][j].Item2;
+                foreach (var antenna in antennas[lines[y][x]])
+                {
+                    var dY = y - antenna.Item1;
+                    var dX = x - antenna.Item2;
 
-                PlaceAntinode(antennas[frequency][i].Item1, antennas[frequency][i].Item2, dY, dX);
-                PlaceAntinode(antennas[frequency][j].Item1, antennas[frequency][j].Item2, 0 - dY, 0 - dX);
+                    PlaceAntinode(y, x, dY, dX);
+                    PlaceAntinode(antenna.Item1, antenna.Item2, 0 - dY, 0 - dX);
+                }
+
+                value.Add(new Tuple<int, int>(y, x));
             }
 
 Console.WriteLine(antinodes.Count);
