0700e6a89b61432d35f52aec75d55129c17f1707
Rob Habraken <roberthabraken@hotmail.com>
2024-12-09 07:09:54 +0100

    Add solution to day 9 part 1

--- solutions/09/part-1/Program.cs
+++ solutions/09/part-1/Program.cs
@@ -1,9 +1,53 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\09\\input.example");
+var diskmap = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\09\\input.txt")[0];
 
-var answer = 0;
-foreach (var line in lines)
+var fileblocks = new List<int>();
+
+var id = 0;
+for (var i = 0; i < diskmap.Length; i++)
 {
-
+    var digit = int.Parse($"{diskmap[i]}");
+    if (i % 2 == 1)
+    {
+        for (var j = 0; j < digit; j++)
+            fileblocks.Add(-1);
+    }
+    else
+    {
+        for (var j = 0; j < digit; j++)
+            fileblocks.Add(id);
+        id++;
+    }
 }
 
-Console.WriteLine(answer);
+for (var i = fileblocks.Count - 1; i >= 0; i--)
+{
+    if (fileblocks[i] >= 0)
+    {
+        var freespace = freeSpaceIndex();
+        if (freespace < 0)
+            break;
+
+        fileblocks[freespace] = fileblocks[i];
+        fileblocks[i] = -1;
+    }
+}
+
+long checksum = 0;
+for (var i = 0; i < fileblocks.Count && fileblocks[i] >= 0; i++)
+    checksum += fileblocks[i] * i;
+
+Console.WriteLine(checksum);
+
+int freeSpaceIndex()
+{
+    var firstEmptyBlockIndex = -1;
+    for (var i = 0; i < fileblocks.Count; i++)
+    {
+        if (firstEmptyBlockIndex < 0 && fileblocks[i] == -1)
+            firstEmptyBlockIndex = i;
+
+        if (firstEmptyBlockIndex >= 0 && fileblocks[i] >= 0)
+            return firstEmptyBlockIndex;
+    }
+    return -1;
+}
