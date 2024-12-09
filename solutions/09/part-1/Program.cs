1347da3719af40fc903e36a0fa64ff2253680586
Rob Habraken <roberthabraken@hotmail.com>
2024-12-09 10:51:41 +0100

    Further refactored and optimized day 9 part 1

--- solutions/09/part-1/Program.cs
+++ solutions/09/part-1/Program.cs
@@ -1,10 +1,9 @@
 var diskmap = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\09\\input.txt")[0];
 
 var fileblocks = new List<int>();
-var leftMostFreeBlock = 0;
+var freespace = 0;
 
-var id = 0;
-for (var i = 0; i < diskmap.Length; i++)
+for (int i = 0, id = 0; i < diskmap.Length; i++)
 {
     var digit = int.Parse($"{diskmap[i]}");
     if (i % 2 == 1)
@@ -21,37 +20,27 @@ for (var i = 0; i < diskmap.Length; i++)
 }
 
 for (var i = fileblocks.Count - 1; i >= 0; i--)
-{
     if (fileblocks[i] >= 0)
     {
-        var freespace = freeSpaceIndex();
+        freespace = freeSpaceIndex(i);
         if (freespace < 0)
             break;
 
         fileblocks[freespace] = fileblocks[i];
         fileblocks[i] = -1;
     }
-}
 
 long checksum = 0;
-for (var i = 0; i < fileblocks.Count && fileblocks[i] >= 0; i++)
+for (var i = 0; fileblocks[i] >= 0; i++)
     checksum += fileblocks[i] * i;
 
 Console.WriteLine(checksum);
 
-int freeSpaceIndex()
+int freeSpaceIndex(int maxIndex)
 {
-    var firstEmptyBlockIndex = -1;
-    for (var i = leftMostFreeBlock; i < fileblocks.Count; i++)
-    {
-        if (firstEmptyBlockIndex < 0 && fileblocks[i] == -1)
-            firstEmptyBlockIndex = i;
+    for (var i = freespace; i < maxIndex; i++)
+        if (fileblocks[i] == -1)
+            return i;
 
-        if (firstEmptyBlockIndex >= 0 && fileblocks[i] >= 0)
-        {
-            leftMostFreeBlock = firstEmptyBlockIndex + 1;
-            return firstEmptyBlockIndex;
-        }
-    }
     return -1;
 }
