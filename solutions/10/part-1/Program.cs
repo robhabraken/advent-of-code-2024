d97540202064d3a93dddd834d1d89e75fc4a3bf1
Rob Habraken <roberthabraken@hotmail.com>
2024-12-10 07:44:17 +0100

    Add solutions to day 10 part 1 and 2

--- solutions/10/part-1/Program.cs
+++ solutions/10/part-1/Program.cs
@@ -1,9 +1,41 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\10\\input.example");
+var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\10\\input.txt");
+
+var deltaMap = new int[4, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+var reachable9s = new HashSet<Tuple<int, int>>();
 
 var answer = 0;
-foreach (var line in lines)
+for (var y =  0; y < lines.Length; y++)
 {
+    for (var x = 0; x < lines[0].Length; x++)
+    {
+        if (lines[y][x].Equals('0'))
+        {
+            Walk(y, x);
 
+            answer += reachable9s.Count;
+            reachable9s = new HashSet<Tuple<int, int>>();
+        }
+    }
 }
 
 Console.WriteLine(answer);
+
+void Walk(int y, int x)
+{
+    var here = int.Parse($"{lines[y][x]}");
+    if (here == 9)
+    {
+        reachable9s.Add(new Tuple<int, int>(y, x));
+        return;
+    }
+
+    for (var i = 0; i < 4; i++)
+    {
+        var dY = y + deltaMap[i, 0];
+        var dX = x + deltaMap[i, 1];
+
+        if (dY >= 0 && dY < lines.Length && dX >= 0 && dX < lines[0].Length)
+            if (int.Parse($"{lines[dY][dX]}") == here + 1)
+                Walk(dY, dX);
+    }
+}
--- solutions/10/part-2/Program.cs
+++ solutions/10/part-2/Program.cs
@@ -1,9 +1,31 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\10\\input.example");
+var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\10\\input.txt");
+
+var deltaMap = new int[4, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
 var answer = 0;
-foreach (var line in lines)
-{
-
-}
+for (var y = 0; y < lines.Length; y++)
+    for (var x = 0; x < lines[0].Length; x++)
+        if (lines[y][x].Equals('0'))
+            Walk(y, x);
 
 Console.WriteLine(answer);
+
+void Walk(int y, int x)
+{
+    var here = int.Parse($"{lines[y][x]}");
+    if (here == 9)
+    {
+        answer++;
+        return;
+    }
+
+    for (var i = 0; i < 4; i++)
+    {
+        var dY = y + deltaMap[i, 0];
+        var dX = x + deltaMap[i, 1];
+
+        if (dY >= 0 && dY < lines.Length && dX >= 0 && dX < lines[0].Length)
+            if (int.Parse($"{lines[dY][dX]}") == here + 1)
+                Walk(dY, dX);
+    }
+}
