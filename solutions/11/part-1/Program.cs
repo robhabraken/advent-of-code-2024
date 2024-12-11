1e3f64f9da0f23f23d1ce0a9594df33d80971c5c
Rob Habraken <roberthabraken@hotmail.com>
2024-12-11 09:14:12 +0100

    Optimize day 11 part 1 for better performance

--- solutions/11/part-1/Program.cs
+++ solutions/11/part-1/Program.cs
@@ -1,30 +1,38 @@
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\11\\input.txt");
 
-
+var cache = new Dictionary<long, List<long>>();
 var stones = lines[0].Split(' ').Select(long.Parse).ToList();
 
+stones.Sort();
 for (var i = 0; i < 25; i++)
 {
     var blink = new List<long>();
     for (var s = 0; s < stones.Count; s++)
-    {
-        var str = $"{stones[s]}";
+        blink.AddRange(breakdownStones(stones[s]));
 
-        if (stones[s] == 0)
-            blink.Add(1);
-        else if (str.Length % 2 == 0)
-        {
-            blink.Add(long.Parse(str[..(str.Length / 2)]));
-            blink.Add(long.Parse(str[(str.Length / 2)..]));
-        }
-        else
-            blink.Add(stones[s] * 2024);
-    }
     stones = blink;
-
-    //foreach (var stone in blink)
-    //    Console.Write($"{stone} ");
-    //Console.WriteLine();
 }
 
 Console.WriteLine(stones.Count);
+
+List<long> breakdownStones(long stoneEngraving)
+{
+    if (cache.TryGetValue(stoneEngraving, out List<long>? value))
+        return value;
+
+    var breakdown = new List<long>();
+
+    var str = $"{stoneEngraving}";
+    if (stoneEngraving == 0)
+        breakdown.Add(1);
+    else if (str.Length % 2 == 0)
+    {
+        breakdown.Add(long.Parse(str[..(str.Length / 2)]));
+        breakdown.Add(long.Parse(str[(str.Length / 2)..]));
+    }
+    else
+        breakdown.Add(stoneEngraving * 2024);
+
+    cache.Add(stoneEngraving, breakdown);
+    return breakdown;
+}
