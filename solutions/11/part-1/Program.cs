c4843981b624a365f1a2a372afe54e9433500a70
Rob Habraken <roberthabraken@hotmail.com>
2024-12-11 15:51:14 +0100

    Renaming of day 11 part 1 to match part 2

--- solutions/11/part-1/Program.cs
+++ solutions/11/part-1/Program.cs
@@ -8,31 +8,31 @@ for (var i = 0; i < 25; i++)
 {
     var blink = new List<long>();
     for (var s = 0; s < stones.Count; s++)
-        blink.AddRange(breakdownStones(stones[s]));
+        blink.AddRange(changeStones(stones[s]));
 
     stones = blink;
 }
 
 Console.WriteLine(stones.Count);
 
-List<long> breakdownStones(long stoneEngraving)
+List<long> changeStones(long engraving)
 {
-    if (cache.TryGetValue(stoneEngraving, out List<long>? value))
+    if (cache.TryGetValue(engraving, out List<long>? value))
         return value;
 
-    var breakdown = new List<long>();
+    var change = new List<long>();
 
-    var str = $"{stoneEngraving}";
-    if (stoneEngraving == 0)
-        breakdown.Add(1);
+    var str = $"{engraving}";
+    if (engraving == 0)
+        change.Add(1);
     else if (str.Length % 2 == 0)
     {
-        breakdown.Add(long.Parse(str[..(str.Length / 2)]));
-        breakdown.Add(long.Parse(str[(str.Length / 2)..]));
+        change.Add(long.Parse(str[..(str.Length / 2)]));
+        change.Add(long.Parse(str[(str.Length / 2)..]));
     }
     else
-        breakdown.Add(stoneEngraving * 2024);
+        change.Add(engraving * 2024);
 
-    cache.Add(stoneEngraving, breakdown);
-    return breakdown;
+    cache.Add(engraving, change);
+    return change;
 }
