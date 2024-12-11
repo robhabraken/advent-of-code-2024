c2f8d521a3ee999150a5dcaafd18ae40c4e630dc
Rob Habraken <roberthabraken@hotmail.com>
2024-12-11 15:48:07 +0100

    Rename variables and clean up code for day 11 part 2

--- solutions/11/part-2/Program.cs
+++ solutions/11/part-2/Program.cs
@@ -1,8 +1,5 @@
-using System.Diagnostics;
-
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\11\\input.txt");
 
-var answer = 0L;
 var cache = new Dictionary<long, List<long>>();
 
 var stones = new Dictionary<long, long>(); // engraving, multiplier
@@ -22,6 +19,7 @@ for (var i = 0; i < 75; i++)
     stones = blink;
 }
 
+var answer = 0L;
 foreach (var stone in stones.Keys)
     answer += stones[stone];
 
@@ -32,19 +30,19 @@ List<long> changeStones(long engraving)
     if (cache.TryGetValue(engraving, out List<long>? value))
         return value;
 
-    var breakdown = new List<long>();
+    var change = new List<long>();
 
     var str = $"{engraving}";
     if (engraving == 0)
-        breakdown.Add(1);
+        change.Add(1);
     else if (str.Length % 2 == 0)
     {
-        breakdown.Add(long.Parse(str[..(str.Length / 2)]));
-        breakdown.Add(long.Parse(str[(str.Length / 2)..]));
+        change.Add(long.Parse(str[..(str.Length / 2)]));
+        change.Add(long.Parse(str[(str.Length / 2)..]));
     }
     else
-        breakdown.Add(engraving * 2024);
+        change.Add(engraving * 2024);
 
-    cache.Add(engraving, breakdown);
-    return breakdown;
+    cache.Add(engraving, change);
+    return change;
 }
