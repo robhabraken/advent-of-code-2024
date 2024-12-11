e6a8c68876849fbbac4bf51e69f61f50b1540546
Rob Habraken <roberthabraken@hotmail.com>
2024-12-11 06:45:26 +0100

    Add solution to day 11 part 1

--- solutions/11/part-1/Program.cs
+++ solutions/11/part-1/Program.cs
@@ -1,9 +1,30 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\11\\input.example");
+var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\11\\input.txt");
 
-var answer = 0;
-foreach (var line in lines)
+
+var stones = lines[0].Split(' ').Select(long.Parse).ToList();
+
+for (var i = 0; i < 25; i++)
 {
+    var blink = new List<long>();
+    for (var s = 0; s < stones.Count; s++)
+    {
+        var str = $"{stones[s]}";
 
+        if (stones[s] == 0)
+            blink.Add(1);
+        else if (str.Length % 2 == 0)
+        {
+            blink.Add(long.Parse(str[..(str.Length / 2)]));
+            blink.Add(long.Parse(str[(str.Length / 2)..]));
+        }
+        else
+            blink.Add(stones[s] * 2024);
+    }
+    stones = blink;
+
+    //foreach (var stone in blink)
+    //    Console.Write($"{stone} ");
+    //Console.WriteLine();
 }
 
-Console.WriteLine(answer);
+Console.WriteLine(stones.Count);
