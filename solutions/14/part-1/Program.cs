c50de81add589a367145964411c50e4bf9131f63
Rob Habraken <roberthabraken@hotmail.com>
2024-12-14 11:55:17 +0100

    Cleaned up solution to day 14 part 1

--- solutions/14/part-1/Program.cs
+++ solutions/14/part-1/Program.cs
@@ -1,7 +1,6 @@
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\14\\input.txt");
 
 var robots = new List<Robot>();
-// var dimensions = new Tuple<int, int>(11, 7);
 var dimensions = new Tuple<int, int>(101, 103);
 
 foreach (var line in lines)
@@ -10,37 +9,11 @@ foreach (var line in lines)
     var pos = input[0].Replace("p=", string.Empty).Split(',').Select(int.Parse).ToArray();
     var vel = input[1].Replace("v=", string.Empty).Split(',').Select(int.Parse).ToArray();
 
-    robots.Add(new Robot() {
-        position = new Tuple<int, int>(pos[0], pos[1]),
-        velocity = new Tuple<int, int>(vel[0], vel[1])
-    });
+    robots.Add(new Robot(new Tuple<int, int>(pos[0], pos[1]), new Tuple<int, int>(vel[0], vel[1])));
 }
 
-//for (var i = 0; i < 100; i++)
-//{
-//    foreach (var robot in robots)
-//        robot.Move(dimensions);
-//}
 foreach (var robot in robots)
     robot.Move(dimensions, 100);
-
-for (var y = 0; y < dimensions.Item2; y++)
-{
-    for (var x = 0; x < dimensions.Item1; x++)
-    {
-        var count = 0;
-        foreach (var robot in robots)
-        {
-            if (robot.position.Item1 == x && robot.position.Item2 == y)
-                count++;
-        }
-        if (count > 0)
-            Console.Write(count);
-        else
-            Console.Write(".");
-    }
-    Console.WriteLine();
-}
 
 var quadrants = new int[4];
 foreach (var  robot in robots)
@@ -55,34 +28,12 @@ foreach (var  robot in robots)
         quadrants[3]++;
 }
 
-foreach (var q in quadrants)
-    Console.WriteLine(q);
-
 Console.WriteLine(quadrants[0] * quadrants[1] * quadrants[2] * quadrants[3]);
 
-class Robot()
+internal class Robot(Tuple<int, int> position, Tuple<int, int> velocity)
 {
-    public Tuple<int, int> position;
-    public Tuple<int, int> velocity;
-
-    public void Move(Tuple<int, int> dimensions)
-    {
-        var pX = position.Item1 + velocity.Item1;
-        var pY = position.Item2 + velocity.Item2;
-
-
-        if (pX < 0)
-            pX += dimensions.Item1;
-        if (pX >= dimensions.Item1)
-            pX -= dimensions.Item1;
-
-        if (pY < 0)
-            pY += dimensions.Item2;
-        if (pY >= dimensions.Item2)
-            pY -= dimensions.Item2;
-
-        position = new Tuple<int, int>(pX, pY);
-    }
+    public Tuple<int, int> position = position;
+    public Tuple<int, int> velocity = velocity;
 
     public void Move(Tuple<int, int> dimensions, int steps)
     {
