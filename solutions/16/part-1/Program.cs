e53246390a6d904f11de1a1381e4484d9d74ed0c
Rob Habraken <roberthabraken@hotmail.com>
2024-12-18 12:14:04 +0100

    Clean up solution of day 16 part 1, fix typo bug, improve naming and formatting

--- solutions/16/part-1/Program.cs
+++ solutions/16/part-1/Program.cs
@@ -3,14 +3,14 @@ var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-i
 var deltaMap = new int[4, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
 var nodes = new List<Node>();
-var start = new Node(new Point(0, 0), true, false);
-var end = new Node(new Point(0, 0), false, true);
+var start = new Node(0, 0, true, false);
+var end = new Node(0, 0, false, true);
 
 for (var y = 0; y < lines.Length; y++)
     for (var x = 0; x < lines[0].Length; x++)
         if (!lines[y][x].Equals('#'))
         {
-            var node = new Node(new Point(x, y), lines[y][x].Equals('S'), lines[y][x].Equals('E'));
+            var node = new Node(x, y, lines[y][x].Equals('S'), lines[y][x].Equals('E'));
             nodes.Add(node);
 
             if (node.start)
@@ -23,28 +23,23 @@ foreach (var node in nodes)
 {
     foreach (var otherNode in nodes)
         if (node != otherNode &&
-            ((Math.Abs(otherNode.p.x - node.p.x) <= 1 && otherNode.p.y == node.p.y) ||
-             (Math.Abs(otherNode.p.y - node.p.y) <= 1) && otherNode.p.x == node.p.x))
+            ((Math.Abs(otherNode.x - node.x) <= 1 && otherNode.y == node.y) ||
+             (Math.Abs(otherNode.y - node.y) <= 1) && otherNode.x == node.x))
         {
             var e = new Edge(otherNode, 1);
 
             for (var i = 0; i < 4; i++)
-                if (otherNode.p.y - node.p.y == deltaMap[i, 0] && otherNode.p.x - node.p.x == deltaMap[i, 1])
+                if (otherNode.y - node.y == deltaMap[i, 0] && otherNode.x - node.x == deltaMap[i, 1])
                     e.direction = i;
 
             node.connections.Add(e);
         }
-    node.totalDistance = straightLineDistance(node, end);
+    node.totalDistance = Math.Sqrt(Math.Pow(node.x - end.x, 2) + Math.Pow(node.y - end.y, 2));
 }
 
 search();
 
 Console.WriteLine(end.minCostToStart);
-
-double straightLineDistance(Node from, Node to)
-{
-    return Math.Sqrt(Math.Pow(from.p.x - to.p.x, 2) + Math.Pow(from.p.y - from.p.y, 2));
-}
 
 void search()
 {
@@ -53,38 +48,40 @@ void search()
     do
     {
         priorityQueue = [.. priorityQueue.OrderBy(x => x.Item1.minCostToStart + x.Item1.totalDistance)];
-        var nodeTuple = priorityQueue.First();
-        priorityQueue.Remove(nodeTuple);
-        foreach (var c in nodeTuple.Item1.connections.OrderBy(x => x.Cost))
+        var nodeWithDirection = priorityQueue.First();
+        priorityQueue.Remove(nodeWithDirection);
+        foreach (var c in nodeWithDirection.Item1.connections.OrderBy(x => x.Cost))
         {
             var connectedNode = c.ConnectedNode;
             if (connectedNode.visited)
                 continue;
 
-            var cost = nodeTuple.Item1.minCostToStart + c.Cost;
-            if (c.direction != nodeTuple.Item2)
+            var cost = nodeWithDirection.Item1.minCostToStart + c.Cost;
+            if (c.direction != nodeWithDirection.Item2)
                 cost += 1000;
 
             if (connectedNode.minCostToStart == null || cost < connectedNode.minCostToStart)
             {
                 connectedNode.minCostToStart = cost;
-                connectedNode.nearestToStart = nodeTuple.Item1;
+                connectedNode.nearestToStart = nodeWithDirection.Item1;
 
                 var newTuple = new Tuple<Node, int>(connectedNode, c.direction);
                 if (!priorityQueue.Contains(newTuple))
                     priorityQueue.Add(newTuple);
             }
 
-            nodeTuple.Item1.visited = true;
+            nodeWithDirection.Item1.visited = true;
         }
-        if (nodeTuple.Item1.end)
+        if (nodeWithDirection.Item1.end)
             return;
-    } while (priorityQueue.Count > 0);
+    }
+    while (priorityQueue.Count > 0);
 }
 
-internal class Node(Point p, bool start, bool end)
+internal class Node(int x, int y, bool start, bool end)
 {
-    public Point p = p;
+    public int x = x;
+    public int y = y;
     public List<Edge> connections = [];
 
     public bool start = start;
@@ -101,10 +98,4 @@ class Edge(Node connectedNode, int cost)
     public int Cost = cost;
     public Node ConnectedNode = connectedNode;
     public int direction;
-}
-
-class Point(int x, int y)
-{
-    public int x = x;
-    public int y = y;
 }
