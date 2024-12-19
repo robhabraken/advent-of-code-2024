3d94a6e3058b38e1498141cf3f95553b7c25ed3d
Rob Habraken <roberthabraken@hotmail.com>
2024-12-19 18:29:04 +0100

    Add solution for day 19 part 1

--- solutions/19/part-1/Program.cs
+++ solutions/19/part-1/Program.cs
@@ -1,9 +1,41 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\19\\input.example");
+var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\19\\input.txt");
 
 var answer = 0;
-foreach (var line in lines)
-{
+var availablePatterns = lines[0].Split(", ");
 
+for (var i = 2; i < lines.Length; i++)
+{
+    var possible = sortTowels(lines[i]);
+    if (possible)
+        answer++;
 }
 
 Console.WriteLine(answer);
+
+bool sortTowels(string design)
+{
+    var selectPatterns = new List<string>();
+    foreach (var pattern in availablePatterns)
+        if (design.Contains(pattern))
+            selectPatterns.Add(pattern);
+
+    var condense = string.Empty.PadLeft(design.Length, '.');
+    for (var i = 0; i < selectPatterns.Count; i++)
+    {
+        var lastIndex = 0;
+        var crop = design;
+        var indexes = new List<int>();
+
+        while (crop.Contains(selectPatterns[i]))
+        {
+            indexes.Add(design.IndexOf(selectPatterns[i], lastIndex));
+            lastIndex += crop.IndexOf(selectPatterns[i]) + selectPatterns[i].Length;
+            crop = crop[(crop.IndexOf(selectPatterns[i]) + selectPatterns[i].Length)..];
+        }
+
+        foreach (var index in indexes)
+            condense = condense[..index] + selectPatterns[i] + condense[(index + selectPatterns[i].Length)..];
+    }
+
+    return !condense.Contains('.');
+}
