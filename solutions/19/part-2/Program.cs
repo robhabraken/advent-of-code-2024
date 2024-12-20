9bbe941161bc5681826a5a16bed81fd9a36a2747
Rob Habraken <roberthabraken@hotmail.com>
2024-12-20 18:07:38 +0100

    Add solution for day 19 part 2

--- solutions/19/part-2/Program.cs
+++ solutions/19/part-2/Program.cs
@@ -25,23 +25,18 @@ long sortTowels(string design)
             {
                 indexes.Add(design.IndexOf(pattern, lastIndex));
                 towels[design.IndexOf(pattern, lastIndex)].Add(pattern);
-                lastIndex = design.IndexOf(pattern, lastIndex) + pattern.Length;
+                lastIndex = design.IndexOf(pattern, lastIndex) + 1;
             }
         }
     }
 
-    var possibilities = new long[towels.Length];
+    var possibilities = new long[design.Length];
     for (var i = towels.Length - 1; i >= 0; i--)
-    {
-        foreach (var pattern in towels[i])
-        {
-            var l = pattern.Length;
-            if (i + l < towels.Length)
-                possibilities[i] += possibilities[i + l];
+        foreach (var towel in towels[i])
+            if (i + towel.Length < design.Length)
+                possibilities[i] += possibilities[i + towel.Length];
             else
                 possibilities[i]++;
-        }
-    }
 
     return possibilities[0];
 }
