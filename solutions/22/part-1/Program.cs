9005b77c966aca244b2ca3a37a347f3e347b8201
Rob Habraken <roberthabraken@hotmail.com>
2024-12-22 06:27:41 +0100

    Add solution for day 22 part 1

--- solutions/22/part-1/Program.cs
+++ solutions/22/part-1/Program.cs
@@ -1,9 +1,29 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\22\\input.example");
+var secrets = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\22\\input.txt").Select(long.Parse).ToArray();
 
-var answer = 0;
-foreach (var line in lines)
+var answer = 0L;
+for (var i = 0; i < secrets.Length; i++)
 {
-
+    for (var j = 0; j < 2000; j++)
+        secrets[i] = pseudo(secrets[i]);
+    answer += secrets[i];
 }
 
 Console.WriteLine(answer);
+
+long pseudo(long secret)
+{
+    secret = mix(secret, secret * 64);
+    secret = prune(secret);
+
+    secret = mix(secret, secret / 32);
+    secret = prune(secret);
+
+    secret = mix(secret, secret * 2048);
+    secret = prune(secret);
+
+    return secret;
+}
+
+long mix(long secret, long value) => secret ^ value;
+
+long prune(long secret) => secret % 16777216;
