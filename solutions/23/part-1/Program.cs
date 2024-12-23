c79ece80db2d521462cc2b23f9895711fca9f4bf
Rob Habraken <roberthabraken@hotmail.com>
2024-12-23 14:39:44 +0100

    Clean up solution for day 23 part 1 and improve readability

--- solutions/23/part-1/Program.cs
+++ solutions/23/part-1/Program.cs
@@ -1,57 +1,41 @@
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\23\\input.txt");
 
-var computers = new Dictionary<string, Computer>(); 
+var computers = new Dictionary<string, Computer>();
 
 foreach (var line in lines)
 {
     var names = line.Split('-');
     foreach (var name in names)
-    {
         if (!computers.ContainsKey(name))
-        {
-            var c = new Computer();
-            c.Name = name;
-            c.connections = new List<Computer>();
-            computers.Add(name, c);
-        }
-    }
+            computers.Add(name, new Computer(name));
 
     computers[names[0]].connections.Add(computers[names[1]]);
     computers[names[1]].connections.Add(computers[names[0]]);
 }
 
 var sets = new HashSet<string>();
-foreach (var c in computers.Keys)
-{
-    foreach (var conn in computers[c].connections)
-    {
-        foreach (var conn2 in computers[c].connections)
-        {
-            if (conn != conn2)
-            {
-                if (conn2.connections.Contains(computers[c]) && conn2.connections.Contains(conn))
+foreach (var computerName in computers.Keys)
+    foreach (var computerConnection in computers[computerName].connections)
+        foreach (var thirdGradeConnection in computers[computerName].connections)
+            if (computerConnection != thirdGradeConnection &&
+                thirdGradeConnection.connections.Contains(computers[computerName]) &&
+                thirdGradeConnection.connections.Contains(computerConnection) &&
+                (computerName.StartsWith("t") || computerConnection.name.StartsWith("t") || thirdGradeConnection.name.StartsWith("t")))
                 {
-                    if (computers[c].Name.StartsWith("t") || conn.Name.StartsWith("t") || conn2.Name.StartsWith("t"))
+                    var list = new List<string>
                     {
-                        var list = new List<string>
-                        {
-                            computers[c].Name,
-                            conn.Name,
-                            conn2.Name
-                        };
-                        list.Sort();
-                        sets.Add($"{list[0]},{list[1]},{list[2]}");
-                    }
+                        computerName,
+                        computerConnection.name,
+                        thirdGradeConnection.name
+                    };
+                    list.Sort();
+                    sets.Add($"{list[0]},{list[1]},{list[2]}");
                 }
-            }
-        }
-    }
-}
 
 Console.WriteLine(sets.Count);
 
-class Computer
+class Computer(string name)
 {
-    public string Name;
-    public List<Computer> connections;
+    public string name = name;
+    public List<Computer> connections = [];
 }
