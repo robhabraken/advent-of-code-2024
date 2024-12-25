0504dc1f27d29fe375cd57deeb8672452d9f5270
Rob Habraken <roberthabraken@hotmail.com>
2024-12-25 10:16:30 +0100

    Remove part 2 boilerplate for day 25

--- solutions/25/part-2/Program.cs
+++
@@ -1,9 +0,0 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\25\\input.example");
-
-var answer = 0;
-foreach (var line in lines)
-{
-
-}
-
-Console.WriteLine(answer);
