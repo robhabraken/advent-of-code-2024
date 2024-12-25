911cac40c5c096387276bd84a7b2d911aeb79aee
Rob Habraken <roberthabraken@hotmail.com>
2024-12-25 10:56:35 +0100

    Work in progress

--- solutions/21/part-2/Program.cs
+++ solutions/21/part-2/Program.cs
@@ -1,4 +1,4 @@
-string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\24\\input.example");
+string[] lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\21\\input.example");
 
 var answer = 0;
 foreach (var line in lines)
