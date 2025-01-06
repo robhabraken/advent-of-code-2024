474e033207b62f0db88fc467e86ebc8d5989726f
Rob Habraken <roberthabraken@hotmail.com>
2025-01-06 03:50:51 +0100

    Optimize day 6 solution for performance (despite a little duplications in code)

--- solutions/06/part-2/Program.cs
+++ solutions/06/part-2/Program.cs
@@ -1,34 +1,62 @@
 var lines = File.ReadAllLines("..\\..\\..\\..\\..\\..\\..\\advent-of-code-2024-io\\06\\input.txt");
 
 (int, int) location = (0, 0);
+var walls = new bool[lines.Length, lines[0].Length];
 var referenceMap = new bool[lines.Length, lines[0].Length];
 var deltaMap = new int[4, 2] { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
 
 var answer = 0;
 for (int y = 0; y < lines.Length; y++)
+{
     if (lines[y].Contains('^'))
         location = (y, lines[y].IndexOf('^'));
 
-walk(ref referenceMap, (location.Item1, location.Item2), -1, -1);
+    for (int x = 0; x < lines[0].Length; x++)
+        if (lines[y][x].Equals('#'))
+            walls[y, x] = true;
+}
 
-var testMap = new bool[lines.Length, lines[0].Length];
+walk(ref referenceMap, (location.Item1, location.Item2));
+
 for (int y = 0; y < lines.Length; y++)
     for (int x = 0; x < lines[0].Length; x++)
         if (referenceMap[y, x])
-            if (walk(ref testMap, (location.Item1, location.Item2), y, x))
+            if (detectLoop((location.Item1, location.Item2), y, x))
                 answer++;
 
 Console.WriteLine(answer);
 
-bool walk(ref bool[,] map, (int, int) location, int obstacleY, int obstacleX)
+void walk(ref bool[,] map, (int, int) location)
 {
-    var visitedDirections = new bool[lines.Length, lines[0].Length, 4];
     var direction = 0;
 
     while (true)
     {
         map[location.Item1, location.Item2] = true;
 
+        var dY = location.Item1 + deltaMap[direction % 4, 0];
+        var dX = location.Item2 + deltaMap[direction % 4, 1];
+
+        if (dY < 0 || dY >= lines.Length || dX < 0 || dX >= lines[0].Length)
+            return; // walking off of the grid
+
+        if (walls[dY, dX])
+            direction++;
+        else
+        {
+            location.Item1 = dY;
+            location.Item2 = dX;
+        }
+    }
+}
+
+bool detectLoop((int, int) location, int obstacleY, int obstacleX)
+{
+    var visitedDirections = new bool[lines.Length, lines[0].Length, 4];
+    var direction = 0;
+
+    while (true)
+    {
         visitedDirections[location.Item1, location.Item2, direction % 4] = true;
 
         var dY = location.Item1 + deltaMap[direction % 4, 0];
@@ -40,7 +68,7 @@ bool walk(ref bool[,] map, (int, int) location, int obstacleY, int obstacleX)
         if (visitedDirections[dY, dX, direction % 4])
             return true; // loop detected
 
-        if (lines[dY][dX].Equals('#') || (dY == obstacleY && dX == obstacleX))
+        if (walls[dY, dX] || (dY == obstacleY && dX == obstacleX))
             direction++;
         else
         {
